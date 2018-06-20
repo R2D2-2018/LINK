35a50cf1fb3f7e526930da6c4010d6f635c74578
Julian van Doorn <gigajulian@gmail.com>
2018-06-20 12:05:52 +0200

    Added new project files for Arduino

---
+++ Sources.cmake
@@ -0,0 +1,18 @@
+# Libraries:
+
+link_libraries (gcc)
+
+set (hwlib ${build_environment}/libraries/hwlib)
+include_directories (${hwlib}/library)
+
+set (catch ${build_environment}/libraries/Catch2)
+include_directories (${catch}/single_include)
+
+# Source Files:
+
+include_directories (lib-slave/)
+include_directories (src/)
+
+set (sources
+
+)

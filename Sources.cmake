b882a3f96140ef4646c6b86adb1ac30de5ed9f11
Julian van Doorn <gigajulian@gmail.com>
2018-06-21 14:21:45 +0200

    Included UART_LIB from build-environment

--- Sources.cmake
+++ Sources.cmake
@@ -8,11 +8,20 @@ include_directories (${hwlib}/library)
 set (catch ${build_environment}/libraries/Catch2)
 include_directories (${catch}/single_include)
 
+set (uart_lib ${build_environment}/libraries/UART_LIB)
+include_directories (${uart_lib}/src)
+
+set (sources ${sources}
+    ${uart_lib}/src/hardware_uart.cpp
+    ${uart_lib}/src/mock_uart.cpp
+    ${uart_lib}/src/uart_connection.cpp
+)
+
 # Source Files:
 
 include_directories (lib-slave/)
 include_directories (src/)
 
-set (sources
+set (sources ${sources}
 
 )

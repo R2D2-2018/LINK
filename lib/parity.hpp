7a8d2a29b419c1c489875804e4dc312bbd9477b7
Julian van Doorn <gigajulian@gmail.com>
2018-06-22 12:33:41 +0200

    Added parity.hpp with calculateParity function and tests

---
+++ lib/parity.hpp
@@ -0,0 +1,43 @@
+#ifndef PARITY_HPP
+#define PARITY_HPP
+
+#include "wrap-hwlib.hpp"
+
+namespace LinkModule {
+/**
+ * @brief Calculates a parity bit for the provided byte, uses EVEN_PARITY for even or odd parity
+ *  
+ * @tparam EVEN_PARITY sets even or odd parity
+ * @param b Byte to calculate parity for
+ * @return high Parity bit is 1
+ * @return low Parity bit is 0
+ */
+template <bool EVEN_PARITY>
+constexpr bool _calculateParity(uint8_t b) {
+    uint8_t highBitCount = 0;
+
+    for (uint8_t i = 0; i < 8; i++) {
+        highBitCount += (b >> i & 1);
+    }
+
+    return (highBitCount & 1) != EVEN_PARITY; // return 1 if highBitCount is even
+}
+
+/**
+ * @brief Calculates a parity bit for the provided byte
+ * 
+ * @details
+ * This function is a wrapper for _calculateParity<true>(b)
+ * for possible easy re-configruation of the CRC parity.
+ * Currently uses even parity.
+ * 
+ * @param b Byte to calculate parity for
+ * @return high Parity bit is 1
+ * @return low Parity bit is 0
+ */
+constexpr bool calculateParity(uint8_t b) {
+    return _calculateParity<true>(b);
+}
+} // namespace LinkModule
+
+#endif // PARITY_HPP
---
+++ test/test_parity.hpp
@@ -0,0 +1,75 @@
+#ifndef TEST_PARITY_HPP
+#define TEST_PARITY_HPP
+
+#include "catch.hpp"
+#include "parity.hpp"
+
+TEST_CASE("Parity Bit Calculation Test _calculateParity<bool EVEN_PARITY>", "[linkmodule][parity]") {
+    SECTION("Even parity 10101010") {
+        REQUIRE(LinkModule::_calculateParity<true>(0b10101010) == 1);
+    }
+
+    SECTION("Even parity 10001110") {
+        REQUIRE(LinkModule::_calculateParity<true>(0b10001110) == 1);
+    }
+
+    SECTION("Even parity 10100011") {
+        REQUIRE(LinkModule::_calculateParity<true>(0b10100011) == 1);
+    }
+
+    SECTION("Even parity 10111000") {
+        REQUIRE(LinkModule::_calculateParity<true>(0b10111000) == 1);
+    }
+
+    SECTION("Odd parity 10101010") {
+        REQUIRE(LinkModule::_calculateParity<false>(0b10101110) == 1);
+    }
+
+    SECTION("Odd parity 10001010") {
+        REQUIRE(LinkModule::_calculateParity<false>(0b10001010) == 1);
+    }
+
+    SECTION("Odd parity 10100010") {
+        REQUIRE(LinkModule::_calculateParity<false>(0b10100010) == 1);
+    }
+
+    SECTION("Odd parity 10101000") {
+        REQUIRE(LinkModule::_calculateParity<false>(0b10101000) == 1);
+    }
+}
+
+TEST_CASE("Parity Bit Calculation Test calculateParity", "[linkmodule][parity]") {
+    SECTION("Even parity 10101010") {
+        REQUIRE(LinkModule::calculateParity(0b10101010) == 1);
+    }
+
+    SECTION("Even parity 10001110") {
+        REQUIRE(LinkModule::calculateParity(0b10001110) == 1);
+    }
+
+    SECTION("Even parity 10100011") {
+        REQUIRE(LinkModule::calculateParity(0b10100011) == 1);
+    }
+
+    SECTION("Even parity 10111000") {
+        REQUIRE(LinkModule::calculateParity(0b10111000) == 1);
+    }
+
+    SECTION("Even parity 10101010") {
+        REQUIRE(LinkModule::calculateParity(0b10101110) == 0);
+    }
+
+    SECTION("Even parity 10001010") {
+        REQUIRE(LinkModule::calculateParity(0b10001010) == 0);
+    }
+
+    SECTION("Even parity 10100010") {
+        REQUIRE(LinkModule::calculateParity(0b10100010) == 0);
+    }
+
+    SECTION("Even parity 10101000") {
+        REQUIRE(LinkModule::calculateParity(0b10101000) == 0);
+    }
+}
+
+#endif // TEST_PARITY_HPP

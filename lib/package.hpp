ca03e1b86364b715582af2ab45a00d3a50f6e65a
Julian van Doorn <gigajulian@gmail.com>
2018-06-25 16:45:18 +0200

    Added package.hpp

--- lib/package.hpp
+++ lib/package.hpp
@@ -1,8 +1,50 @@
 #ifndef PACKAGE_HPP
 #define PACKAGE_HPP
 
+#include "wrap-hwlib.hpp"
+#include "uart_lib.hpp"
+#include "uart_util.hpp"
+
+#include <array>
+
 namespace LinkModule {
-class Package {};
+template <uint32_t SIZE>
+class SizedPackage {
+  public:
+    std::array<uint8_t, SIZE> data;
+
+    SizedPackage() : data() { }
+    SizedPackage(const std::array<uint8_t, SIZE>& data) : data(data) { }
+
+    void sendPackage(UARTLib::UARTConnection& os) {
+        for (uint32_t i = 0; i < SIZE; i++) {
+            os << data[i];
+        }
+    }
+
+    bool receivePackage(UARTLib::UARTConnection& is, uint64_t timeoutStamp) {
+        for (uint32_t i = 0; i < SIZE; i++) {
+            if (!waitForChar(is, timeoutStamp)) {
+                return false;
+            }
+
+            is >> data[i];
+        }
+
+        return true;
+    }
+
+    template <class OS>
+    friend OS& operator << (OS& os, const SizedPackage& sizedPackage) {
+        for (uint8_t d : sizedPackage.data) {
+            os << (int) d;
+        }
+
+        return os;
+    }
+};
+
+using Package = SizedPackage<16>;
 } // namespace LinkModule
 
 #endif // PACKAGE_HPP
